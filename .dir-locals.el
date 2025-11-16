c1e98ac30d6c1f82f8ba977a9c7d12ec7d66fd8a
Wan Ardie <wan_ahmad_ardie@yahoo.com>
2025-11-16 15:10:37 +0800

    missing programmers run variables/parameters

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+
+(
+ ;; (nil . ((indent-tabs-mode . t)
+ ;;         (tab-width . 4)
+ ;;         (fill-column . 80)))
+ 
+ (c++-mode . ((ardie/project-shell-command . "make clean; qmake main.pro ; make; ./hello")
+              ;; (subdirs . nil)
+	      ))
+ ;; ("src/docs" . ((nil . ((buffer-read-only . t)))))
+ )
